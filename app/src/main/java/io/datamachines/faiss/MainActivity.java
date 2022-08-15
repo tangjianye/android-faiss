bc25d6fbcae0f86c2cd4117cca04dc304ad2e7ba
tangjianye <tangjianye@wegooooo.com>
2022-08-15 17:39:34 +0800

    init

--- app/src/main/java/io/datamachines/faiss/MainActivity.java
+++ app/src/main/java/io/datamachines/faiss/MainActivity.java
@@ -1,10 +1,17 @@
 package io.datamachines.faiss;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -58,12 +65,69 @@ public class MainActivity extends AppCompatActivity {
 
 
         StartTest = findViewById(R.id.button);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 10);
+//            } else {
+//                handlePreViewCallBack();
+//            }
+//        } else {
+//            handlePreViewCallBack();
+//        }
+
+        //========================================
+        //      动态获取权限
+        //========================================
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                //需要的权限
+                String[] permArr = {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                };
+                boolean needReq = false;
+                for (int i = 0; i < permArr.length; i++) {
+                    if (ContextCompat.checkSelfPermission(this, permArr[i]) != PackageManager.PERMISSION_GRANTED) {
+                        needReq = true;
+                        break;
+                    }
+                }
+
+                if (needReq) {
+                    ActivityCompat.requestPermissions(this, permArr, 10);
+                } else {
+                    handlePreViewCallBack();
+                }
+            } catch (Exception e) {
+                Log.e("动态申请权限时，发生异常。", e.toString());
+            }
+        } else {
+            handlePreViewCallBack();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (10 == requestCode) {
+            if (grantResults.length == 2
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                handlePreViewCallBack();
+            } else {
+                Toast.makeText(this, "没有获得必要的权限", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void handlePreViewCallBack() {
         StartTest.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 StartTesting();
             }
         });
-
     }
 
     public void UpdateGUI() {
